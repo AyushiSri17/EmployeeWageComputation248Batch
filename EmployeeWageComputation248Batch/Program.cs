6aee27b52f795e35caea7d3d54bba58f4cba9e0f
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-01-20 19:18:38 +0530

    [Ayushi] Modified And Calculating EmployeeAttendence

--- EmployeeWageComputation248Batch/Program.cs
+++ EmployeeWageComputation248Batch/Program.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeWageComputation248Batch
+namespace EmployeeWageComputation
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To EmployeeWageComputation Program");
+            int isPresent = 1;
+            Console.WriteLine("Welcome to EmployeeWage Computation");
+
+            //UC1 - EmployeeAttendance
+            Random random = new Random();
+            //Next(maxValue) method is used , here we can use Next(minValue,maxValue) also
+            int empAttendance = random.Next(2);
+            Console.WriteLine(empAttendance+"\n");
+            if (empAttendance == isPresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
             Console.ReadLine();
         }
     }
