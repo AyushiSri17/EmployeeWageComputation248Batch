520490261d7ca1a28180865127461ffc0316f81f
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-01-22 21:52:57 +0530

    [Ayushi] Added Calculating Wage Till A Condition Reached

--- EmployeeWageComputation248Batch/Program.cs
+++ EmployeeWageComputation248Batch/Program.cs
@@ -6,21 +6,22 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageComputation
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int MAX_WORKING_DAYS = 20;
+            const int MAX_WORKING_HRS = 100;
             const int EMP_RATE_PER_HR = 20;
-            int empHrs = 0, empWage = 0, day = 1, totalWage = 0;
+            int empHrs = 0, empWage = 0, day = 1, totalWage = 0, totalHrs = 0;
 
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
 
-            //UC5 - Calculate Wage For a Month
+            //UC6 - Calculate Wage Till Condition Reached
             Random random = new Random();
-            for (day = 1; day <= MAX_WORKING_DAYS; day++)
+            while (day <= MAX_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
             {
                 int empAttendance = random.Next(0, 3);
                 switch (empAttendance)
@@ -39,10 +40,12 @@ namespace EmployeeWageComputation
                         break;
                 }
                 empWage = empHrs * EMP_RATE_PER_HR;
-                Console.WriteLine("Day {0} Employee Wage:{1}", day, empWage);
+                Console.WriteLine("Day {0} Employee Wage: {1}", day, empWage);
                 totalWage += empWage;
+                day++;
+                totalHrs += empHrs;
             }
-            Console.WriteLine("Total Employee Wage for {0} days: {1}", (day - 1), totalWage);
+            Console.WriteLine("\nTotal Employee Wage for {0} days: {1} and total working hours {2}", (day - 1), totalWage, totalHrs);
             Console.ReadLine();
         }
     }
