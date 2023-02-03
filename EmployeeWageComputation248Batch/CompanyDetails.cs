e37dee5c6c593efd05f0292b1c470cdca65d2885
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-02-03 20:35:44 +0530

    [Ayushi] Refactor to manage employee wage of multiple companies

---
+++ EmployeeWageComputation248Batch/CompanyDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation248Batch
+{
+    public class CompanyDetails
+    {
+        public string company;
+        public int maxWorkingDays, maxWorkingHrs, empRatePerHr, totalWage;
+
+        public CompanyDetails(string company, int maxWorkingDays, int maxWorkingHrs, int empRatePerHr)
+        {
+            this.company = company;
+            this.maxWorkingDays = maxWorkingDays;
+            this.maxWorkingHrs = maxWorkingHrs;
+            this.empRatePerHr = empRatePerHr;
+        }
+        public void SetTotalWage(int totalWage)
+        {
+            this.totalWage = totalWage;
+        }
+        public override string ToString()
+        {
+            return $"Company:{company} TotalWage:{totalWage}";
+        }
+    }
+}
--- EmployeeWageComputation248Batch/EmployeeWageBuilder.cs
+++ EmployeeWageComputation248Batch/EmployeeWageBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeWageComputation248Batch;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,37 +14,48 @@ namespace EmployeeWageComputation
         //const int MAX_WORKING_DAYS = 20;
         //const int MAX_WORKING_HRS = 100;
         //const int EMP_RATE_PER_HR = 20;
-        public string company;
-        public int maxWorkingDays, maxWorkingHrs, empRatePerHr;
+        CompanyDetails[] companies;
+        int numOfCompany = 0;
 
         //constructor
-        public EmployeeWageBuilder(string company, int maxWorkingDays, int maxWorkingHrs, int empRatePerHr)
+        public EmployeeWageBuilder()
         {
-            this.company = company;
-            this.maxWorkingDays = maxWorkingDays;
-            this.maxWorkingHrs= maxWorkingHrs;
-            this.empRatePerHr = empRatePerHr;
+            companies= new CompanyDetails[3];
+        }
+        public void AddCompanyDetails(string company, int maxWorkingDays, int maxWorkingHrs, int empRatePerHr)
+        {
+          CompanyDetails companyDetails = new CompanyDetails(company,maxWorkingDays,maxWorkingHrs,empRatePerHr);
+            companies[numOfCompany] = companyDetails;
+            numOfCompany++;
+        }
+
+        public void IterateOverCompanies()
+        {
+            for(int i=0;i<companies.Length;i++) 
+            {
+                int totalWage = ComputeEmployeeWage(companies[i]);
+                companies[i].SetTotalWage(totalWage);
+                //Console.WriteLine(companies[i].company + " " + companies[i].totalWage);
+                Console.WriteLine(companies[i]);
+            }
         }
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
-
-            EmployeeWageBuilder deloitte = new EmployeeWageBuilder("BridgeLabz", 20, 100, 35);
-            deloitte.ComputeEmployeeWage();
-            EmployeeWageBuilder bridge = new EmployeeWageBuilder("Google", 24, 45, 30);
-            bridge.ComputeEmployeeWage();
-            EmployeeWageBuilder google = new EmployeeWageBuilder("MicroSoft", 18, 55, 40);
-            google.ComputeEmployeeWage();
-            
+            EmployeeWageBuilder builder = new EmployeeWageBuilder();
+            builder.AddCompanyDetails("BridgeLabz", 20, 100, 35);
+            builder.AddCompanyDetails("Google", 24, 45, 30);
+            builder.AddCompanyDetails("MicroSoft", 18, 55, 40);
+            builder.IterateOverCompanies();                                   
             Console.ReadLine();
         }
-        public void ComputeEmployeeWage()
+        public int ComputeEmployeeWage(CompanyDetails details)
         {    
             int empHrs = 0, empWage = 0, day = 1, totalWage = 0, totalHrs = 0;
 
             //UC6 - Calculate Wage Till Condition Reached
             Random random = new Random();
-            while (day <= maxWorkingDays && totalHrs <= maxWorkingHrs)
+            while (day <= details.maxWorkingDays && totalHrs <= details.maxWorkingHrs)
             {
                 int empAttendance = random.Next(0, 3);
                 switch (empAttendance)
@@ -61,14 +73,14 @@ namespace EmployeeWageComputation
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * empRatePerHr;
+                empWage = empHrs * details.empRatePerHr;
                 //Console.WriteLine("Day {0} Employee Wage: {1}", day, empWage);
                 totalWage += empWage;
                 day++;
                 totalHrs += empHrs;
             }
-            Console.WriteLine("\nTotal Employee Wage for {3} {0} days: is {1} and total working hours {2}", (day - 1), totalWage, totalHrs, company);
-
+            //Console.WriteLine("\nTotal Employee Wage for {3} {0} days: is {1} and total working hours {2}", (day - 1), totalWage, totalHrs, details.company);
+            return totalWage;
         }
     }
 }
